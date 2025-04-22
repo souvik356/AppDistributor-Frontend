5bad3e8eba28593fa1ba18be1d2a6a3847480e9f
shivani <shivanihgf7@gmail.com>
2025-04-22 10:57:40 +0530

    added svg icons and made changes to header

--- src/components/CompName.jsx
+++ src/components/CompName.jsx
@@ -1,36 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { GoHome } from "react-icons/go";
-import { useLocation } from 'react-router-dom';
-
+import { useLocation } from "react-router-dom";
+import { GrAppsRounded } from "react-icons/gr";
+import AppIcon from "../assets/AppIcon1.svg";
 const CompName = () => {
-    const [compName,setCompName] = useState('')
-    const location = useLocation()
-    // console.log(location);
-    const {pathname} = location
-    // console.log(pathname);
+  const [compName, setCompName] = useState("");
+  const location = useLocation();
+  // console.log(location);
+  const { pathname } = location;
+  // console.log(pathname);
 
-    useEffect(()=>{
-        if(pathname === '/dashboard'){
-            setCompName('Dashboard')
-        }
-        else if(pathname === '/dashboard/application'){
-            setCompName('Application')
-        }
-        else if(pathname === '/dashboard/release'){
-            setCompName('Release')
-        }
-    },[pathname])
-    
+  useEffect(() => {
+    if (pathname === "/dashboard") {
+      setCompName("Dashboard");
+    } else if (pathname === "/dashboard/application") {
+      setCompName("Application");
+    } else if (pathname === "/dashboard/release") {
+      setCompName("Release");
+    }
+  }, [pathname]);
+
+  console.log(compName);
   return (
-    <div className=' pt-22 h-50 flex items-center justify-between px-6 rounded-bl-4xl rounded-br-4xl bg-white'>
-        <div>
-           <h1 className='text-5xl font-semibold'>{compName}</h1>
-        </div>
-        <div>
-            <GoHome size={80}/>
-        </div>
+    <div className=" pt-22 h-50 flex items-center justify-between px-6 rounded-bl-4xl rounded-br-4xl bg-white">
+      <div>
+        <h1 className="text-5xl font-semibold">{compName}</h1>
+      </div>
+      <div>
+        {compName == "Dashboard" ? <GoHome size={80} /> : <img src={AppIcon} />}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CompName
+export default CompName;
--- src/components/Release.jsx
+++ src/components/Release.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import searchIcon from "../assets/searchIcon.svg";
+import filterIcon from "../assets/filterIcon.svg";
+import goBackIcon from "../assets/GoBackIcon.svg";
 const Release = () => {
-    
   return (
-    <div>Release</div>
-  )
-}
+    <div>
+      <div className="flex justify-between m-6">
+        <div className="flex justify-between items-center  ">
+          <img src={goBackIcon} className="mr-5"></img>
+          <div className="flex justify-between px-8 py-1.5 mr-5 rounded-xl text-black bg-[#3777F61A] cursor-pointer">
+            <input placeholder="Search Release" className="outline-none" />
+            <img src={searchIcon}></img>
+          </div>
 
-export default Release
+          <div className="p-4  rounded-xl  bg-[#3777F61A] cursor-pointer">
+            <img src={filterIcon}></img>
+          </div>
+        </div>
+        <button className="px-8 py-1.5  rounded-xl text-black bg-[#3777F61A] cursor-pointer">
+          Add Release
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Release;
